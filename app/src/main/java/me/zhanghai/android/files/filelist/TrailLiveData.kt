64c0a5c293cd135ff1ab452a04a202dc2e321982
Goooler <wangzongler@gmail.com>
2022-03-21 15:54:10 +0800

    注释 TrailLiveData

--- app/src/main/java/me/zhanghai/android/files/filelist/TrailLiveData.kt
+++ app/src/main/java/me/zhanghai/android/files/filelist/TrailLiveData.kt
@@ -10,6 +10,10 @@ import androidx.lifecycle.LiveData
 import java8.nio.file.Path
 
 class TrailLiveData : LiveData<TrailData>() {
+    /**
+     * 导航到某路径
+     * @param path 该路径
+     */
     fun navigateTo(lastState: Parcelable, path: Path) {
         val oldTrailData = value
         if (oldTrailData == null) {
@@ -19,10 +23,17 @@ class TrailLiveData : LiveData<TrailData>() {
         value = oldTrailData.navigateTo(lastState, path)
     }
 
+    /**
+     * 重置到某路径
+     * @param path 该路径
+     */
     fun resetTo(path: Path) {
         value = TrailData.of(path)
     }
 
+    /**
+     * 返回上一级
+     */
     fun navigateUp(): Boolean {
         val oldTrailData = value ?: return false
         val trailData = oldTrailData.navigateUp() ?: return false
@@ -30,6 +41,9 @@ class TrailLiveData : LiveData<TrailData>() {
         return true
     }
 
+    /**
+     * 刷新
+     */
     fun reload() {
         value = value
     }
